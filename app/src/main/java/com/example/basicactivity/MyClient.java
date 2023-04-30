56242fb50220ad63990caefdbfbce3c03cf4d027
lribeirogome <lise.gomes@etudiant.univ-rennes1.fr>
2023-05-01 00:14:14 +0200

    mise à jour des commentaires MyClient.java + traduction

--- app/src/main/java/com/example/basicactivity/MyClient.java
+++ app/src/main/java/com/example/basicactivity/MyClient.java
@@ -4,69 +4,61 @@ import java.net.*;
 import java.io.*;
 
 public class MyClient {
+    public static String data = "Empty";
+    public static Boolean change = false; // Indicates whether or not data has changed since last iteration
+    public static Boolean isConnected = false; // Client connected or not
 
-    // Déclaration des variables statiques
-    public static String str = "Empty"; // Chaîne de caractères initiale
-    public static Boolean change= false; // Indique si la chaîne de caractères a changé depuis la dernière itération
-    public static Boolean isConnected =false; // Indique si le client est connecté au serveur
-
-    // Méthode pour lancer le client
-    public static void launchClient(){
-
+    public static void launchClient() {
         Thread thread = new Thread(() -> {
             try  {
                 // Connexion au serveur
-                //Socket s=new Socket("10.0.2.2",3333); //localhost
-                Socket s=new Socket("10.3.141.1",3333);       //Vega
-                DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+                // Socket s = new Socket("10.0.2.2",3333); //localhost
+                Socket socket = new Socket("10.3.141.1",3333);
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-                // Affichage d'un message de connexion réussie
                 System.out.println("Connected to server.");
-                isConnected=true;
+                isConnected = true;
 
-                // Boucle principale pour envoyer des données au serveur
                 while(true){
-
                     if(change) {
-                        // Affichage de la chaîne de caractères actuelle
-                        System.out.println("str = " + str);
-                        dout.writeUTF(str); // Envoi de la chaîne de caractères au serveur
-                        dout.flush();
+                        System.out.println("data : " + data);
+                        out.writeUTF(data); // Send data to server
+                        out.flush();
 
-                        // Si la chaîne de caractères est "stop", arrêter la connexion
-                        if(str.equals("stop")){
+                        // Stops connexion
+                        if(data.equals("stop")){
                             isConnected=false;
-                            str="Empty";
+                            data ="Empty";
                             break;
                         }
 
-                        change = false; // La chaîne de caractères a été envoyée
+                        change = false;
                     }
                 }
 
-                // Fermeture des flux de données et de la connexion
-                dout.close();
-                s.close();
+                out.close();
+                socket.close();
             } catch (Exception e) {
                 System.out.println("Failed to connect to server.");
             }
         });
 
-        // Démarrage du thread pour le client
         thread.start();
 
     }
 
-    // Méthode pour obtenir l'état de connexion du client
     public static Boolean getIsConnected() {
         return isConnected;
     }
 
-    // Méthode pour définir la chaîne de caractères actuelle et signaler un changement
-    public static String setStr(String str) {
-        MyClient.str = str;
+    /**
+     * Define new data to be sent and signals change
+     * @param data data to be sent
+     * @return data sent
+     */
+    public static String setStr(String data) {
+        MyClient.data = data;
         change = true;
-        return str;
+        return data;
     }
-
 }
